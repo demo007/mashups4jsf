78ecf1d48267707dd2cfdf4fb60ace77d389c0ea
Hazem.saleh <Hazem.saleh@93e6ebd0-82a3-11dd-82a6-aba1b78984bd>
2009-11-26 16:35:15 +0000

    Updating the examples to include the youTube search service examples.

--- trunk/examples/src/main/java/com/googlecode/mashups4jsf/example/beans/WeatherBean.java
+++ trunk/examples/src/main/java/com/googlecode/mashups4jsf/example/beans/WeatherBean.java
@@ -15,27 +15,4 @@ public class WeatherBean {
 	public void setSelectedLocation(String selectedLocation) {
 		this.selectedLocation = selectedLocation;
 	}
-
-	public String getCurrentWeatherStatus() {
-//		String feedResult = null;
-//
-//		try {
-//
-//			WeatherServiceStatus weatherServiceStatus = MashupFactory.getYahooWeatherService().getWeather(selectedLocation, 'c');
-//			feedResult = weatherServiceStatus.getTitle()
-//					   + "<br>"
-//					   + weatherServiceStatus.getDescription();
-//
-//			if (feedResult.indexOf("Invalid") >= 0) {
-//				return "";
-//			}
-//
-//		} catch (Exception e) {
-//			e.printStackTrace();
-//		}
-//
-//		return feedResult;
-	    
-	    return "";
-	}
 }
