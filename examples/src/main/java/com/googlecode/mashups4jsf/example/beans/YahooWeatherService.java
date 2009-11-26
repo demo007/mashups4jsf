4e98fc6c90fb9c062b7162b643fe9ca7c1e07aa6
Hazem.saleh <Hazem.saleh@93e6ebd0-82a3-11dd-82a6-aba1b78984bd>
2009-11-26 14:45:04 +0000

    Updating the examples to include the youTube search service examples.

--- examples/src/main/java/com/googlecode/mashups4jsf/example/beans/YahooWeatherService.java
+++
@@ -1,5 +0,0 @@
-package com.googlecode.mashups4jsf.example.beans;
-
-public interface YahooWeatherService {
-	public WeatherServiceStatus getWeather(String zipCode, char temperatureType) throws Exception;
-}
