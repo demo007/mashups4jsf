096cb17ac115e9c4f3e7c263d02106046b231214
Hazem.saleh <Hazem.saleh@93e6ebd0-82a3-11dd-82a6-aba1b78984bd>
2009-11-26 14:45:04 +0000

    Updating the examples to include the youTube search service examples.

--- trunk/examples/src/main/java/com/googlecode/mashups4jsf/example/beans/MashupFactory.java
+++
@@ -1,7 +0,0 @@
-package com.googlecode.mashups4jsf.example.beans;
-
-public class MashupFactory {
-	public static YahooWeatherService getYahooWeatherService () {
-		return new YahooWeatherServiceImpl();
-	}
-}
--- trunk/examples/src/main/java/com/googlecode/mashups4jsf/example/beans/YahooWeatherServiceImpl.java
+++
@@ -1,9 +0,0 @@
-package com.googlecode.mashups4jsf.example.beans;
-
-public class YahooWeatherServiceImpl implements YahooWeatherService {
-	private final static String YAHOO_WEATHER_SERVICE_URL = "http://weather.yahooapis.com/forecastrss";
-
-	public WeatherServiceStatus getWeather(String zipCode, char temperatureType) throws Exception {
-		return null;
-	}
-}
