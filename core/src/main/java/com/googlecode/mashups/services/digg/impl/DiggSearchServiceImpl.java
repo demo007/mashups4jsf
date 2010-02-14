fc3657e87abdff7cea96fe18ab0f32f5c8dad9b7
Hazem.saleh <Hazem.saleh@93e6ebd0-82a3-11dd-82a6-aba1b78984bd>
2010-02-14 20:23:04 +0000

    Enhancing the Digg example thanks to Mohamed Maher.

--- core/src/main/java/com/googlecode/mashups/services/digg/impl/DiggSearchServiceImpl.java
+++ core/src/main/java/com/googlecode/mashups/services/digg/impl/DiggSearchServiceImpl.java
@@ -34,8 +34,9 @@ import com.googlecode.mashups.services.digg.api.DiggSearchStoryResultItem;
 import com.googlecode.mashups.services.digg.api.DiggSearchStoryResults;
 import com.googlecode.mashups.services.digg.exceptions.InvalidResultException;
 
+/* Implements DiggSearchService methods */
 public class DiggSearchServiceImpl implements DiggSearchService {
-    
+
     // XML Tags for Digg Stories
     private static final String DIGG_STORIES_ELEMENT          = "stories";
     private static final String DIGG_STORY_ELEMENT            = "story";
@@ -45,14 +46,14 @@ public class DiggSearchServiceImpl implements DiggSearchService {
     private static final String DIGG_TOPIC_ELEMENT            = "topic";
     private static final String DIGG_CONTAINER_ELEMENT        = "container";
     private static final String DIGG_SHORTURL_ELEMENT         = "shorturl";
-    
+
     // Stories Elements Attributes
     private static final String STORIES_COUNT_ATTRIBUTE       = "count";
     private static final String STORIES_OFFSET_ATTRIBUTE      = "offset";
     private static final String STORIES_TIME_ATTRIBUTE        = "timestamp";
     private static final String STORIES_TOTAL_ATTRIBUTE       = "total";
     private static final String STORIES_VERSION_ATTRIBUTE     = "version";
-    
+
     // Story Element Attributes
     private static final String STORY_LINK_ATTRIBUTE          = "link";
     private static final String STORY_SUBMITDATE_ATTRIBUTE    = "submit_date";
@@ -62,20 +63,20 @@ public class DiggSearchServiceImpl implements DiggSearchService {
     private static final String STORY_HREF_ATTRIBUTE          = "href";
     private static final String STORY_STATUS_ATTRIBUTE        = "status";
     private static final String STORY_MEDIA_ATTRIBUTE         = "media";
-    
+
     // User Element Attributes
-    private static final String USER_ICON_ATTRIBUTE           = "userIcon";
+    private static final String USER_ICON_ATTRIBUTE           = "icon";
     private static final String USER_NAME_ATTRIBUTE           = "name";
     private static final String USER_PROFILEVIEWS_ATTRIBUTE   = "profileviews";
     private static final String USER_REGISTERED_ATTRIBUTE     = "registered";
-    
+
     // Topic Element Attributes
     private static final String TOPIC_NAME_ATTRIBUTE          = "name";
     private static final String TOPIC_SHORTNAME_ATTRIBUTE     = "short_name";
 
     private static final String CONTAINER_NAME_ATTRIBUTE      = "name";
     private static final String CONTAINER_SHORTNAME_ATTRIBUTE = "short_name";
-    
+
     // ShortUSRL
     private static final String SHORTURL_URL_ATTRIBUTE        = "short_url";
     private static final String SHORTURL_VIEW_COUNT_ATTRIBUTE = "view_count";
@@ -84,45 +85,60 @@ public class DiggSearchServiceImpl implements DiggSearchService {
         return diggSearchService;
     }
 
-    public DiggSearchStoryResults getStoriesList(List<ServiceParameter> parameters) throws Exception {
-
-        // Prepare search story parameters
-        parameters = prepareSearchStoryQueryParameters(parameters);
-
-        // Prepare the search url
-        URL searchUrl = new URL(DIGG_SEARCH_SERVICE_URL + "?"
-                + ServiceParametersUtility.toParametersString(parameters));
-        SAXBuilder builder = new SAXBuilder();
-        Document document = builder.build(searchUrl);
-        Element storiesElement = document.getRootElement();
+    @SuppressWarnings("unchecked")
+    public DiggSearchStoryResults getStoriesList(
+            List<ServiceParameter> parameters) throws Exception {
         
+        DiggSearchStoryResults results = new DiggSearchStoryResults();
+        SAXBuilder builder=null;
+        Document document = null;
+        Element storiesElement = null;
+
+        /* Prepare search story parameters */
+        parameters = prepareSearchStoryQueryParameters(parameters);
+        
+        /* Safely prepare the search URL and make connection */
+        try {
+            URL searchUrl = new URL(DIGG_SEARCH_SERVICE_URL + "?"
+                    + ServiceParametersUtility.toParametersString(parameters));
+            builder  = new SAXBuilder();
+            document = builder.build(searchUrl);
+            storiesElement = document.getRootElement();
+        } catch (Exception e) {
+            e.printStackTrace();
+            return null;
+        }
+       
+        // Check for invalid result or returned an error
         if (!DIGG_STORIES_ELEMENT.equals(storiesElement.getName())) {
             throw new InvalidResultException(
-                    "The result root element is expected to be (" + DIGG_STORIES_ELEMENT
-							+ ") but returned element is (" + storiesElement + ")");
+                    "The result root element is expected to be ("
+                            + DIGG_STORIES_ELEMENT
+                            + ") but returned element is (" + storiesElement
+                            + ")");
         }
-
+        
+        /*  Parse the Results as long there is no returned Error code */
+        // Start retrieving the results root element
         List<Element> storyList = storiesElement.getChildren(DIGG_STORY_ELEMENT);
 
-        DiggSearchStoryResults results = new DiggSearchStoryResults();
-        
-        // parse here
+        // Start parsing for general results data
         results.setCount(storiesElement.getAttributeValue(STORIES_COUNT_ATTRIBUTE));
         results.setOffset(storiesElement.getAttributeValue(STORIES_OFFSET_ATTRIBUTE));
         results.setTotal(storiesElement.getAttributeValue(STORIES_TOTAL_ATTRIBUTE));
         results.setTimestamp(storiesElement.getAttributeValue(STORIES_TIME_ATTRIBUTE));
         results.setVersion(storiesElement.getAttributeValue(STORIES_VERSION_ATTRIBUTE));
-        System.out.println("Stories :" + results);
-
+        
+        // Start collecting the list of results
         List<DiggSearchStoryResultItem> searchResultList = new ArrayList<DiggSearchStoryResultItem>();
 
         // Add each parsed story
         for (Element element : storyList) {
-        
+
             // Cursor element
             Element cur = null;
             DiggSearchStoryResultItem resultItem = new DiggSearchStoryResultItem();
-            
+
             // Story Element
             resultItem.setStoryComments(element.getAttributeValue(STORY_COMMENTS_ATTRIBUTE));
             resultItem.setStoryDiggs(element.getAttributeValue(STORY_DIGGS_ATTRIBUTE));
@@ -138,10 +154,9 @@ public class DiggSearchServiceImpl implements DiggSearchService {
 
             // Title
             resultItem.setTitle(element.getChild(DIGG_TITLE_ELEMENT).getText());
-            
+
             // User Info
             cur = element.getChild(DIGG_USER_ELEMENT);
-            
             resultItem.setUserIcon(cur.getAttributeValue(USER_ICON_ATTRIBUTE));
             resultItem.setUserName(cur.getAttributeValue(USER_NAME_ATTRIBUTE));
             resultItem.setUserProfileviews(cur.getAttributeValue(USER_PROFILEVIEWS_ATTRIBUTE));
@@ -149,46 +164,41 @@ public class DiggSearchServiceImpl implements DiggSearchService {
 
             // Topic
             cur = element.getChild(DIGG_TOPIC_ELEMENT);
-            
             resultItem.setTopicName(cur.getAttributeValue(TOPIC_NAME_ATTRIBUTE));
             resultItem.setTopicShortName(cur.getAttributeValue(TOPIC_SHORTNAME_ATTRIBUTE));
 
             // Container
             cur = element.getChild(DIGG_CONTAINER_ELEMENT);
-            
             resultItem.setContainerName(cur.getAttributeValue(CONTAINER_NAME_ATTRIBUTE));
             resultItem.setContainerShortName(cur.getAttributeValue(CONTAINER_SHORTNAME_ATTRIBUTE));
 
             // Short_url
             cur = element.getChild(DIGG_SHORTURL_ELEMENT);
-            
             resultItem.setShorturl(cur.getAttributeValue(SHORTURL_URL_ATTRIBUTE));
             resultItem.setShorturlViewCount(cur.getAttributeValue(SHORTURL_VIEW_COUNT_ATTRIBUTE));
-            
-            //System.out.println(resultItem);
-            
+
             // Add the populated item to the resultList
             searchResultList.add(resultItem);
         }
-        
+
         results.setSearchResultList(searchResultList);
-        
+
         return results;
     }
 
     /** Prepare and normalize the required parameter before submitting */
     private List<ServiceParameter> prepareSearchStoryQueryParameters(
             List<ServiceParameter> parameters) {
-        
-        List<ServiceParameter> supportedParameters = new ArrayList<ServiceParameter>();
-        supportedParameters.add(new ServiceParameter(
-                DiggSearchServiceParameters.SEARCH_ARGS_METHOD,
-                DiggSearchService.DIGG_SEARCH_STORY_METHOD));
-        supportedParameters.add(new ServiceParameter(
-                DiggSearchServiceParameters.SEARCH_ARGS_TYPE,
-                DiggSearchService.DIGG_SEARCH_STORY_TYPE));
 
-        // filter the supported argument from the parameters
+        List<ServiceParameter> supportedParameters = new ArrayList<ServiceParameter>();
+
+        /* Required API parameters: Implicit fixed value parameters to the search API */
+        supportedParameters.add(new ServiceParameter(
+                DiggSearchServiceParameters.SEARCH_ARGS_METHOD, DiggSearchService.DIGG_SEARCH_STORY_METHOD));
+        supportedParameters.add(new ServiceParameter(
+                DiggSearchServiceParameters.SEARCH_ARGS_TYPE,DiggSearchService.DIGG_SEARCH_STORY_TYPE));
+
+        /* Optional API parameters: filters the supported argument from the submitted parameters */ 
         for (ServiceParameter param : parameters) {
             if (param.getParameterName().equals(
                     DiggSearchServiceParameters.SEARCH_ARGS_COUNT)) {
