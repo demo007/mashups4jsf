05ede27bf6467f09cf044a6b7d158445ec3caa56
hazem.saleh <hazem.saleh@93e6ebd0-82a3-11dd-82a6-aba1b78984bd>
2010-06-04 16:19:36 +0000

    re-adding tests.

---
+++ core/src/test/java/com/googlecode/mashups/services/RssFeedProducerServiceTest.java
@@ -0,0 +1,49 @@
+/*
+ * Licensed to the Apache Software Foundation (ASF) under one
+ * or more contributor license agreements.  See the NOTICE file
+ * distributed with this work for additional information
+ * regarding copyright ownership.  The ASF licenses this file
+ * to you under the Apache License, Version 2.0 (the
+ * "License"); you may not use this file except in compliance
+ * with the License.  You may obtain a copy of the License at
+ *
+ *   http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing,
+ * software distributed under the License is distributed on an
+ * "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY
+ * KIND, either express or implied.  See the License for the
+ * specific language governing permissions and limitations
+ * under the License.
+ */
+package com.googlecode.mashups.services.test;
+
+import java.io.FileWriter;
+import java.io.Writer;
+
+import junit.framework.TestCase;
+
+import com.googlecode.mashups.services.factory.GenericServicesFactory;
+
+/**
+ * @author hazems
+ *
+ */
+public class RssFeedProducerServiceTest extends TestCase {
+
+    public void testGetRssFeed() throws Exception {
+	try {
+	    News news = new News();
+	    
+	    Writer writer = new FileWriter("c:\\feed.xml");
+	    
+	    GenericServicesFactory.getFeedProducerService().produceFeed(news, writer);
+	    
+	    writer.close();
+
+	} catch (Exception e) {
+	    e.printStackTrace();
+	    fail("Unable to get the Rss Feed producer ...");
+	}
+    }
+}
