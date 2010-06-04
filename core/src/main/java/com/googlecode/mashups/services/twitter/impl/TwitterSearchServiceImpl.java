745f1fc9c9de0a8ec5b2c34347951648cf72f598
hazem.saleh <hazem.saleh@93e6ebd0-82a3-11dd-82a6-aba1b78984bd>
2010-06-04 15:59:26 +0000

    Adding the twitter service.

--- core/src/main/java/com/googlecode/mashups/services/twitter/impl/TwitterSearchServiceImpl.java
+++ core/src/main/java/com/googlecode/mashups/services/twitter/impl/TwitterSearchServiceImpl.java
@@ -18,23 +18,78 @@
  */
 package com.googlecode.mashups.services.twitter.impl;
 
+import java.net.URL;
+import java.util.ArrayList;
 import java.util.List;
 
+import org.jdom.Document;
+import org.jdom.Element;
+import org.jdom.Namespace;
+import org.jdom.input.SAXBuilder;
+
+import com.googlecode.mashups.services.common.ServiceConstants;
 import com.googlecode.mashups.services.common.ServiceParameter;
+import com.googlecode.mashups.services.common.ServiceParametersUtility;
 import com.googlecode.mashups.services.twitter.api.TwitterSearchResultItem;
 import com.googlecode.mashups.services.twitter.api.TwitterSearchService;
 
 public class TwitterSearchServiceImpl implements TwitterSearchService {
     public static TwitterSearchService getInstance() {
-        return twitterSearchService;
+    return twitterSearchService;
     }
 
-	public List<TwitterSearchResultItem> getUpdatesList(List<ServiceParameter> parameters) throws Exception {
-		return null;
-	}    
-    
-    private TwitterSearchServiceImpl() {
+    @SuppressWarnings("unchecked")    
+    public List<TwitterSearchResultItem> getSearchList(List<ServiceParameter> parameters) throws Exception {
+        parameters = prepareQueryParameters(parameters);
+        
+        URL                           feedUrl       = new URL(TWITTER_SEARCH_SERVICE_URL
+                                                    + "?"
+                                                    + ServiceParametersUtility.toParametersString(parameters));
+        SAXBuilder                    builder       = new SAXBuilder();
+        Document                      document      = builder.build(feedUrl);
+        Namespace 		      namespace     = Namespace.getNamespace(ServiceConstants.ATOM_FEED, ServiceConstants.ATOM_NAMESPACE_URI);        
+        List<TwitterSearchResultItem> searchResults = new ArrayList<TwitterSearchResultItem>();
+        List<Element>                 children      = document.getRootElement().getChildren(ServiceConstants.ATOM_ENTRY, namespace);;
+        
+        for (Element element : children) {
+            String titleNotFormatted = element.getChildText(ServiceConstants.TITLE, namespace);
+            String title             = element.getChildText(ServiceConstants.ATOM_CONTENT, namespace);         
+            String publishedDate     = element.getChildText(ServiceConstants.ATOM_PUB_DATE, namespace);            
+            String updateDate        = element.getChildText(ServiceConstants.ATOM_UPDATE_DATE, namespace);            
+            String authorName        = element.getChild(ServiceConstants.AUTHOR, namespace).getChildText(ServiceConstants.ATOM_AUTHOR_NAME, namespace);
+            String authorTwitterURL  = element.getChild(ServiceConstants.AUTHOR, namespace).getChildText(ServiceConstants.ATOM_URI, namespace);  
+            String authorImageURL    = "";
+            
+            List links = element.getChildren(ServiceConstants.LINK, namespace);
+            
+            for (int i = 0; i < links.size(); ++i) {
+        	Element linkElement = (Element) links.get(i);
+        	
+        	if (linkElement.getAttributeValue(ServiceConstants.TYPE).contains(ServiceConstants.IMAGE)) {
+        	    authorImageURL = linkElement.getAttributeValue(ServiceConstants.HREF);
+        	}
+            }
+            
+            searchResults.add(new TwitterSearchResultItem(title, titleNotFormatted, authorName, authorTwitterURL, authorImageURL, publishedDate, updateDate));       
+        }
+        
+        return searchResults;
     }
     
+    /*
+     * @param parameters
+     * @return
+     */
+    private List<ServiceParameter> prepareQueryParameters(List<ServiceParameter> parameters) {
+        if (parameters == null) {
+            parameters = new ArrayList<ServiceParameter>();
+        }
+
+        return parameters;
+    }        
+
+    private TwitterSearchServiceImpl() {
+    }
+
     private static TwitterSearchService twitterSearchService = new TwitterSearchServiceImpl();
 }
