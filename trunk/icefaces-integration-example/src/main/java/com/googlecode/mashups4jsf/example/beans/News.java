91c01d1d1a7fe895d0be06408935df168b49ed87
hazem.saleh <hazem.saleh@93e6ebd0-82a3-11dd-82a6-aba1b78984bd>
2011-04-02 18:13:18 +0000

    Adding implicit @feed.

--- trunk/icefaces-integration-example/src/main/java/com/googlecode/mashups4jsf/example/beans/News.java
+++ trunk/icefaces-integration-example/src/main/java/com/googlecode/mashups4jsf/example/beans/News.java
@@ -20,12 +20,12 @@ package com.googlecode.mashups4jsf.example.beans;
 
 import java.util.ArrayList;
 import java.util.List;
+
 import com.googlecode.mashups.services.generic.api.Feed;
 import com.googlecode.mashups.services.generic.api.FeedDescription;
 import com.googlecode.mashups.services.generic.api.FeedItems;
 import com.googlecode.mashups.services.generic.api.FeedLink;
 import com.googlecode.mashups.services.generic.api.FeedTitle;
-import com.googlecode.mashups.services.generic.api.Feed.FeedType;
 
 
 /**
@@ -33,7 +33,7 @@ import com.googlecode.mashups.services.generic.api.Feed.FeedType;
  *
  */
 
-@Feed(type=FeedType.Rss)
+@Feed
 public class News {
     static List <NewsItem> news = new ArrayList<NewsItem>();
     
