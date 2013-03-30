8bca92cb5d03e53246032041fbc2a2f38206db1c
hazem.saleh@gmail.com <hazem.saleh@gmail.com@93e6ebd0-82a3-11dd-82a6-aba1b78984bd>
2013-03-30 03:15:17 +0000

    Maximizing the flexibility of Mashup feed producing. Resolving issue number: Issue 36.

--- trunk/core/src/main/java/com/googlecode/mashups/services/generic/impl/FeedProducerImpl.java
+++ trunk/core/src/main/java/com/googlecode/mashups/services/generic/impl/FeedProducerImpl.java
@@ -23,9 +23,11 @@ import java.lang.reflect.Method;
 import java.util.ArrayList;
 import java.util.List;
 
+import com.googlecode.mashups.services.generic.api.Feed;
 import com.googlecode.mashups.services.generic.api.FeedDescription;
 import com.googlecode.mashups.services.generic.api.FeedItems;
 import com.googlecode.mashups.services.generic.api.FeedLink;
+import com.googlecode.mashups.services.generic.api.FeedProducer;
 import com.googlecode.mashups.services.generic.api.FeedTitle;
 import com.googlecode.mashups.services.generic.api.FeedType;
 import com.googlecode.mashups.services.generic.api.ItemAuthor;
@@ -33,9 +35,6 @@ import com.googlecode.mashups.services.generic.api.ItemCategory;
 import com.googlecode.mashups.services.generic.api.ItemDescription;
 import com.googlecode.mashups.services.generic.api.ItemLink;
 import com.googlecode.mashups.services.generic.api.ItemTitle;
-import com.googlecode.mashups.services.generic.api.Feed;
-import com.googlecode.mashups.services.generic.api.FeedProducer;
-import com.googlecode.mashups.services.generic.api.FeedItem;
 import com.sun.syndication.feed.synd.SyndCategory;
 import com.sun.syndication.feed.synd.SyndCategoryImpl;
 import com.sun.syndication.feed.synd.SyndContent;
@@ -60,9 +59,9 @@ public class FeedProducerImpl implements FeedProducer {
     @SuppressWarnings("unchecked")
     public void produceFeed(FeedType feedType, Object annotatedObject, Writer writer) throws Exception {
         Class  feedClass      = annotatedObject.getClass();
-        String title          = "";
-        String description    = "";
-        String link           = "";
+        String title          = null;
+        String description    = null;
+        String link           = null;
         List   feedItems      = new ArrayList();
         Feed   feedAnnotation = (Feed) feedClass.getAnnotation(Feed.class);
 
@@ -82,6 +81,32 @@ public class FeedProducerImpl implements FeedProducer {
             throw new Exception("Object must have the @Feed for producing an RSS Feed ...");
         }
         
+        // Try to look for the feed automatically ...
+        if (title == null) {
+        	try {
+        		title = (String) annotatedObject.getClass().getMethod("getTitle").invoke(annotatedObject);
+        	} catch (Exception exception) {
+        		title = "Default Title";
+        	}
+        }        
+        
+        if (description == null) {
+        	try {
+        		description = (String) annotatedObject.getClass().getMethod("getDescription").invoke(annotatedObject);
+        	} catch (Exception exception) {
+        		description = "Default Description";
+        	}
+        }
+        
+        if (link == null) {
+        	try {
+        		link = (String) annotatedObject.getClass().getMethod("getLink").invoke(annotatedObject);
+        	} catch (Exception exception) {
+        		link = "http://";
+        	}
+        }        
+        
+        // Construct feed object.
         SyndFeed feed = new SyndFeedImpl();
         
         if (feedType.equals(FeedType.RSS)) {
@@ -94,6 +119,7 @@ public class FeedProducerImpl implements FeedProducer {
         feed.setDescription(description);
         feed.setLink(link);
         
+        // Construct feed items.
         List<SyndEntry> entries = new ArrayList<SyndEntry>();
 
         for (Object feedItem : feedItems) {
@@ -113,51 +139,89 @@ public class FeedProducerImpl implements FeedProducer {
     private SyndEntry createFeedItem(Object annotatedObject) throws Exception {
         SyndEntry syndEntry     = new SyndEntryImpl();
         Class     feedItemClass = annotatedObject.getClass();
+      
+        String title        = null;
+        String description  = null;            
+        String link         = null;
+        String category     = null;         
+        String author       = null;                     
         
-        if (feedItemClass.getAnnotation(FeedItem.class) != null) {
-            String title        = "";
-            String description  = "";            
-            String link         = "";
-            String category     = "";         
-            String author       = "";                     
+        for (Method method : feedItemClass.getMethods()) {
             
-            for (Method method : feedItemClass.getMethods()) {
-                
-                if (method.getAnnotation(ItemTitle.class) != null) {
-                    title = (String) method.invoke(annotatedObject);
-                } else if (method.getAnnotation(ItemDescription.class) != null) {
-                    description = (String) method.invoke(annotatedObject);
-                } else if (method.getAnnotation(ItemLink.class) != null) {
-                    link = (String) method.invoke(annotatedObject);
-                } else if (method.getAnnotation(ItemCategory.class) != null) {
-                    category = (String) method.invoke(annotatedObject);
-                } else if (method.getAnnotation(ItemAuthor.class) != null) {
-                    author = (String) method.invoke(annotatedObject);
-                }
+            if (method.getAnnotation(ItemTitle.class) != null) {
+                title = (String) method.invoke(annotatedObject);
+            } else if (method.getAnnotation(ItemDescription.class) != null) {
+                description = (String) method.invoke(annotatedObject);
+            } else if (method.getAnnotation(ItemLink.class) != null) {
+                link = (String) method.invoke(annotatedObject);
+            } else if (method.getAnnotation(ItemCategory.class) != null) {
+                category = (String) method.invoke(annotatedObject);
+            } else if (method.getAnnotation(ItemAuthor.class) != null) {
+                author = (String) method.invoke(annotatedObject);
             }
-            
-            // category.
-            List<SyndCategory> categories = new ArrayList<SyndCategory>();
-            
-            SyndCategory syndCategory = new SyndCategoryImpl();
-            syndCategory.setName(category);
-            categories.add(syndCategory);
-            
-            syndEntry.setCategories(categories);
-            
-            // description.
-            SyndContent descriptionContent = new SyndContentImpl();
-            
-            descriptionContent.setType("text/html");
-            descriptionContent.setValue(description);
-            syndEntry.setDescription(descriptionContent);
-            
-            
-            // title, link, author, categories.
-            syndEntry.setTitle(title);
-            syndEntry.setLink(link);
-            syndEntry.setAuthor(author);
         }
+        
+        // Try to look for the feed item values automatically ...
+        if (title == null) {
+        	try {
+        		title = (String) annotatedObject.getClass().getMethod("getTitle").invoke(annotatedObject);
+        	} catch (Exception exception) {
+        		title = "Default Title";
+        	}
+        }
+        
+        if (description == null) {
+        	try {
+        		description = (String) annotatedObject.getClass().getMethod("getDescription").invoke(annotatedObject);
+        	} catch (Exception exception) {
+        		description = "Default Description";
+        	}
+        }
+        
+        if (link == null) {
+        	try {
+        		link = (String) annotatedObject.getClass().getMethod("getLink").invoke(annotatedObject);
+        	} catch (Exception exception) {
+        		link = "http://";
+        	}
+        }
+        
+        if (category == null) {
+        	try {
+        		category = (String) annotatedObject.getClass().getMethod("getCategory").invoke(annotatedObject);
+        	} catch (Exception exception) {
+        		category = "Default Category";
+        	}
+        }
+        
+        if (author == null) {
+        	try {
+        		author = (String) annotatedObject.getClass().getMethod("getAuthor").invoke(annotatedObject);
+        	} catch (Exception exception) {
+        		author = "Default Author";
+        	}
+        }            
+        
+        // category.
+        List<SyndCategory> categories = new ArrayList<SyndCategory>();
+        
+        SyndCategory syndCategory = new SyndCategoryImpl();
+        syndCategory.setName(category);
+        categories.add(syndCategory);
+        
+        syndEntry.setCategories(categories);
+        
+        // description.
+        SyndContent descriptionContent = new SyndContentImpl();
+        
+        descriptionContent.setType("text/html");
+        descriptionContent.setValue(description);
+        syndEntry.setDescription(descriptionContent);
+        
+        // title, link, author, categories.
+        syndEntry.setTitle(title);
+        syndEntry.setLink(link);
+        syndEntry.setAuthor(author);
         
         return syndEntry;
     }     
